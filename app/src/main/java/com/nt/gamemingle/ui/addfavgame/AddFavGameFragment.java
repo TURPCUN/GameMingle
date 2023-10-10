7cd73779e28e9c825ab3bc00042424180c41da2a
Neslihan TURPCU <neslihanaydin43@gmail.com>
2023-10-10 10:54:03 -0700

    GM-25 Design UI for adding favorite board games

---
+++ app/src/main/java/com/nt/gamemingle/ui/addfavgame/AddFavGameFragment.java
@@ -0,0 +1,36 @@
+package com.nt.gamemingle.ui.addfavgame;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nt.gamemingle.R;
+
+public class AddFavGameFragment extends Fragment {
+    public AddFavGameFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_fav_game, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
