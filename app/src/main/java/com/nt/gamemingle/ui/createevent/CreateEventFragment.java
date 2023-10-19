a27865eadc7500116ce7e0f4fbaff736a044789a
Neslihan TURPCU <neslihanaydin43@gmail.com>
2023-10-19 16:26:22 -0700

    GM-29 Implemented event creation logic

--- app/src/main/java/com/nt/gamemingle/ui/createevent/CreateEventFragment.java
+++ app/src/main/java/com/nt/gamemingle/ui/createevent/CreateEventFragment.java
@@ -4,26 +4,38 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.google.android.material.datepicker.CalendarConstraints;
 import com.google.android.material.datepicker.DateValidatorPointForward;
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.timepicker.MaterialTimePicker;
 import com.google.android.material.timepicker.TimeFormat;
+import com.nt.gamemingle.R;
 import com.nt.gamemingle.databinding.FragmentCreateEventBinding;
+import com.nt.gamemingle.model.BoardGame;
 import com.nt.gamemingle.ui.common.BaseFragment;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 public class CreateEventFragment extends BaseFragment {
 
     private CreateEventViewModel mViewModel;
 
     FragmentCreateEventBinding binding;
+
+    String formattedDate = "", formattedTime = "";
+    private List<BoardGame> boardGameList = new ArrayList<>();
+
 
     public CreateEventFragment() {
         // Required empty public constructor
@@ -55,6 +67,43 @@ public class CreateEventFragment extends BaseFragment {
                 showMaterialDatePicker();
             }
         });
+
+        binding.removeTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formattedDate = "";
+                formattedTime = "";
+                binding.linearLayoutShowTime.setVisibility(View.GONE);
+                binding.linearLayoutAddTime.setVisibility(View.VISIBLE);
+            }
+        });
+
+        binding.btnCreateEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performCreateEvent();
+            }
+        });
+
+        mViewModel.getBoardGames(requireContext());
+        final Observer<List<BoardGame>> isBoardGameLoaded = new Observer<List<BoardGame>>() {
+
+            @Override
+            public void onChanged(List<BoardGame> boardGamesLiveData) {
+                if (boardGamesLiveData != null) {
+                    boardGameList = mViewModel.boardGamesLiveDataCreateEvent.getValue();
+                    String[] boardGamesNamesArray = new String[boardGameList.size()];
+                    for (int i = 0; i < boardGameList.size(); i++) {
+                        boardGamesNamesArray[i] = boardGameList.get(i).getGameName();
+                    }
+                    // AutoCompleteTextView for cities
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_dropdown_item_1line, boardGamesNamesArray);
+                    binding.autoCompleteTextViewBoardGame.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        };
+        mViewModel.boardGamesLiveDataCreateEvent.observe(getViewLifecycleOwner(), isBoardGameLoaded);
     }
 
     private void showMaterialDatePicker(){
@@ -76,7 +125,12 @@ public class CreateEventFragment extends BaseFragment {
             // Respond to positive button click.
             Calendar calendar = Calendar.getInstance();
             calendar.setTimeInMillis((Long) selection);
-            binding.textViewShowTime.setText((calendar.get(Calendar.DAY_OF_MONTH)+1) + "/" + (calendar.get(Calendar.MONTH)+1) + "/" + calendar.get(Calendar.YEAR));
+
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+            String formattedDate = sdf.format(calendar.getTime());
+            binding.textViewShowTime.setText(formattedDate);
+            this.formattedDate = formattedDate;
             showMaterialTimePicker();
         });
     }
@@ -95,9 +149,56 @@ public class CreateEventFragment extends BaseFragment {
 
         picker.addOnPositiveButtonClickListener(selection -> {
             // Respond to positive button click.
-            binding.textViewShowTime.setText(binding.textViewShowTime.getText() + " " + picker.getHour() + ":" + picker.getMinute());
+
+            int hour = picker.getHour();
+            int minute = picker.getMinute();
+
+            String formattedTime = String.format(Locale.getDefault(), "%02d:%02d", hour, minute);
+
+            binding.textViewShowTime.setText(binding.textViewShowTime.getText() + " - " + formattedTime);
             binding.linearLayoutShowTime.setVisibility(View.VISIBLE);
             binding.linearLayoutAddTime.setVisibility(View.GONE);
+            this.formattedTime = formattedTime;
         });
     }
+
+    private void performCreateEvent(){
+
+        String eventName = binding.eventName.getText().toString();
+        String eventLocation = binding.location.getText().toString();
+        String eventDescription = binding.description.getText().toString();
+        String selectedGame = binding.autoCompleteTextViewBoardGame.getText().toString();
+        if(eventName.isEmpty()){
+            binding.eventName.setError("Event name is required");
+            return;
+        }
+        if(eventLocation.isEmpty()){
+            binding.location.setError("Event location is required");
+            return;
+        }
+        if(eventDescription.isEmpty()){
+            binding.description.setError("Event description is required");
+            return;
+        }
+        if(formattedDate.isEmpty()){
+            binding.textViewShowTime.setError("Event date is required");
+            return;
+        }
+        if(formattedTime.isEmpty()){
+            binding.textViewShowTime.setError("Event time is required");
+            return;
+        }
+        if(selectedGame.isEmpty()){
+            binding.autoCompleteTextViewBoardGame.setError("Board game is required");
+            return;
+        }
+        String gameId = "";
+        for (int i = 0; i < boardGameList.size(); i++) {
+            if(boardGameList.get(i).getGameName().equals(selectedGame)){
+                gameId = boardGameList.get(i).getBoardGameId();
+            }
+        }
+        mViewModel.createEvent(eventName, eventLocation, eventDescription, gameId, formattedDate, formattedTime);
+    }
+
 }
--- app/src/main/java/com/nt/gamemingle/ui/createevent/CreateEventViewModel.java
+++ app/src/main/java/com/nt/gamemingle/ui/createevent/CreateEventViewModel.java
@@ -1,13 +1,124 @@
 package com.nt.gamemingle.ui.createevent;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.nt.gamemingle.app.AppViewModel;
+import com.nt.gamemingle.model.BoardGame;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
 
 public class CreateEventViewModel {
 
     private AppViewModel appViewModel;
+    MutableLiveData<List<BoardGame>> boardGamesLiveDataCreateEvent = new MutableLiveData<>();
 
     public CreateEventViewModel(AppViewModel appViewModel) {
         super();
         this.appViewModel = appViewModel;
+    }
+
+    public void createEvent(String eventName, String eventLocation, String eventDescription, String selectedGame, String formattedDate, String formattedTime) {
+
+        // EVENT
+        UUID uuid = UUID.randomUUID();
+        String eventUuid = uuid.toString();
+        appViewModel.databaseReference.child("EVENT").child(eventUuid).child("ownerId").setValue(appViewModel.mAuth.getCurrentUser().getUid());
+        appViewModel.databaseReference.child("EVENT").child(eventUuid).child("eventName").setValue(eventName);
+        appViewModel.databaseReference.child("EVENT").child(eventUuid).child("location").setValue(eventLocation);
+        appViewModel.databaseReference.child("EVENT").child(eventUuid).child("description").setValue(eventDescription);
+        appViewModel.databaseReference.child("EVENT").child(eventUuid).child("gameId").setValue(selectedGame);
+        appViewModel.databaseReference.child("EVENT").child(eventUuid).child("date").setValue(formattedDate);
+        appViewModel.databaseReference.child("EVENT").child(eventUuid).child("time").setValue(formattedTime);
+
+    }
+
+    public void getBoardGamess(Context context) {
+        ArrayList<BoardGame> tempBoardGameList = new ArrayList<>();
+        DatabaseReference gamesReference = appViewModel.database.getReference("Games");
+        gamesReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    //BoardGame boardGame = dataSnapshot.getValue(BoardGame.class);
+                    //boardGameList.add(boardGame);
+                    HashMap<String, String> game = (HashMap<String, String>) dataSnapshot.getValue();
+
+                    String gameId = dataSnapshot.getKey();
+                    String gameName = game.get("gameName");
+                    String gameDescription = game.get("gameDescription");
+                    String gameImageUrl = game.get("gameImageUrl"); // TODO, it should come from firebase storage
+                    String gameMinPlayers = game.get("minPlayer").toString();
+                    String gameMaxPlayers = game.get("maxPlayer").toString();
+                    String gameCategory = game.get("gameCategory");
+                    BoardGame boardGame = new BoardGame(gameId, gameName, gameDescription, gameImageUrl, gameMinPlayers, gameMaxPlayers, gameCategory);
+                    tempBoardGameList.add(boardGame);
+                }
+                boardGamesLiveDataCreateEvent.setValue(tempBoardGameList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(context, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void getBoardGames(Context context) {
+        String userId = appViewModel.mAuth.getCurrentUser().getUid();
+        if(userId != null){
+            ArrayList<BoardGame> myGamesLibrary = new ArrayList<>();
+            DatabaseReference userGamesReference = appViewModel.database.getReference("USER_GAME").child(userId);
+            userGamesReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot gameSnapshot : snapshot.getChildren()) {
+                        String gameId = gameSnapshot.getKey();
+                        Boolean inLibrary = gameSnapshot.child("inLibrary").getValue(Boolean.class);
+                        if (gameId != null && inLibrary == true) {
+                            DatabaseReference gamesReference = appViewModel.database.getReference("Games").child(gameId);
+                            gamesReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot gameSnapshot) {
+                                    if (gameSnapshot.exists()) {
+                                        String gameName = gameSnapshot.child("gameName").getValue(String.class);
+                                        String gameDescription = gameSnapshot.child("gameDescription").getValue(String.class);
+                                        String gameImageUrl = gameSnapshot.child("gameImageUrl").getValue(String.class);
+                                        String gameMinPlayers = gameSnapshot.child("minPlayer").getValue(String.class);
+                                        String gameMaxPlayers = gameSnapshot.child("maxPlayer").getValue(String.class);
+                                        String gameCategory = gameSnapshot.child("gameCategory").getValue(String.class);
+
+                                        BoardGame boardGame = new BoardGame(gameId, gameName, gameDescription, gameImageUrl, gameMinPlayers, gameMaxPlayers, gameCategory);
+                                        myGamesLibrary.add(boardGame);
+                                        boardGamesLiveDataCreateEvent.setValue(myGamesLibrary);
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+                                    Toast.makeText(context, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(context, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
     }
 }
